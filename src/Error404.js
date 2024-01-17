52a844a9d621d4769a6485044cb5f8f4a9c1793b
yanisquinto <yanis.quinto@mail.ch>
2024-01-17 20:06:31 +0100

    projet terminé

--- src/Error404.js
+++ src/Error404.js
@@ -4,15 +4,13 @@ import Footer from './components/Footer';
 
 function Error404() {
   return (
-    <div>
-      <Header />
       <div className='container-404'>
+        <Header />
         <h1 className='title-404'>404</h1>
         <p className='noexist-404'>Oups! La page que vous demandez n'existe pas.</p>
         <a href='/' className='gotoindex-404'>Retourner sur la page d'accueil</a>
-      </div>
       <Footer />
-    </div>
+      </div>
   );
 }
 
